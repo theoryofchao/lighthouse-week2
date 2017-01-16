9ef914be7949f4b17edecba12d8c990e5f8248d9
theoryofchao <roger.ti.chao@gmail.com>
2017-01-16 11:48:04 -0500

    step 2 appending to variable and ending to show output

--- https-request-exercise/https-request-exercise.js
+++ https-request-exercise/https-request-exercise.js
@@ -14,7 +14,26 @@ var getAndPrintHtmlChunks = function() {
   });
 };
 
-getAndPrintHtmlChunks();
+var getAndPrintHTML = function () {
+  var requestOptions = {
+    host: `sytantris.github.io`,
+    path: `/http-examples/step2.html`
+  };
+
+  https.get(requestOptions, (response) => {
+    response.setEncoding(`utf8`);
+    var responseString = "";
+    response.on(`data`, (data) => {
+      responseString+=data.toString();
+    });
+
+    response.on(`end`, () => {
+      console.log(responseString);
+    });
+  });
+};
+
+getAndPrintHTML();
 
 /*var requestOptions = {
   host: `example.com`,
